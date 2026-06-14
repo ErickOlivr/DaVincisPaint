cf066e1ba7441ac38548ff0b1da7cd5acef6ed8c
MicaelyCacho <tab.micaelycacho@gmail.com>
2026-06-14 14:53:44 -0300

    feat(lista-encadeada): estrutura básica da classe NoPonto

--- src/main/java/com/paintvetorial/model/NoPonto.java
+++ src/main/java/com/paintvetorial/model/NoPonto.java
@@ -1,4 +1,12 @@
 package com.paintvetorial.model;
 
 public class NoPonto {
+    int x, y;
+    NoPonto proximo;
+    
+    public NoPonto(int x, int y){
+        this.x = x;
+        this.y = y;
+        this.proximo = null;
+    }
 }
