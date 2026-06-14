5aeddf571ef36d035f08fc18634058d35b89552c
MicaelyCacho <tab.micaelycacho@gmail.com>
2026-06-14 15:46:16 -0300

    fix(lista-encadeada): conserta sintaxe da clase NoTraco.java

--- src/main/java/com/paintvetorial/model/NoTraco.java
+++ src/main/java/com/paintvetorial/model/NoTraco.java
@@ -7,28 +7,28 @@ public class NoTraco {
     NoPonto noCabeca;
     NoTraco proximoTraco;
     NoTraco tracoAnterior;
-}
 
-public NoTraco (Color cor, int espressura){
-    this.cor = cor;
-    this.espressura = espressura;
-    this.noCabeca = null;
-    this.proximoTraco = null;
-    this.tracoAnterior = null;
-}
 
-public void addPonto(int x, int y) {
-        NoPonto novoPonto = new NoPonto(x, y);
-
-        if (noCabeca == null) {
-            noCabeca = novoPonto;
-        } else {
-            NoPonto atual = noCabeca;
-            
-            while (atual.proximo != null) {
-                atual = atual.proximo;
-            }
-            atual.proximo = novoPonto;
-        }
+    public NoTraco (Color cor, int espessura){
+        this.cor = cor;
+        this.espessura = espessura;
+        this.noCabeca = null;
+        this.proximoTraco = null;
+        this.tracoAnterior = null;
     }
+
+    public void addPonto(int x, int y) {
+            NoPonto novoPonto = new NoPonto(x, y);
+    
+            if (noCabeca == null) {
+                noCabeca = novoPonto;
+            } else {
+                NoPonto atual = noCabeca;
+                
+                while (atual.proximo != null) {
+                    atual = atual.proximo;
+                }
+                atual.proximo = novoPonto;
+            }
+        }
 }
