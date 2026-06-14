8f05e6e9b74e99e4b38b9d858471c066f63c89ea
MicaelyCacho <tab.micaelycacho@gmail.com>
2026-06-14 15:14:10 -0300

    feat(lista-encadeada): adiciona estrutura da classe NoTraco

--- src/main/java/com/paintvetorial/model/NoTraco.java
+++ src/main/java/com/paintvetorial/model/NoTraco.java
@@ -1,4 +1,34 @@
 package com.paintvetorial.model;
+import java.awt.Color;
 
 public class NoTraco {
+    Color cor;
+    int espessura;
+    NoPonto noCabeca;
+    NoTraco proximoTraco;
+    NoTraco tracoAnterior;
+}
+
+public NoTraco (Color cor, int espressura){
+    this.cor = cor;
+    this.espressura = espressura;
+    this.noCabeca = null;
+    this.proximoTraco = null;
+    this.tracoAnterior = null;
+}
+
+public void addPonto(int x, int y) {
+        NoPonto novoPonto = new NoPonto(x, y);
+
+        if (noCabeca == null) {
+            noCabeca = novoPonto;
+        } else {
+            NoPonto atual = noCabeca;
+            
+            while (atual.proximo != null) {
+                atual = atual.proximo;
+            }
+            atual.proximo = novoPonto;
+        }
+    }
 }
